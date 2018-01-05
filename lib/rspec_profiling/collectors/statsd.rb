f82e52ec293d452867a813ba0792b1732e2e2c2e
Matt Foster <mattfoster01@gmail.com>
2018-01-05 11:24:55 -0500

    STAAS-8929-8931
    * Creates V14 for STAAS-8928 STAAS-8929 STAAS-8930 and STAAS-8931
    
    * STAAS-8929: Adds max depth parameter for formatFile function. Enables flattening of structure
    
    * STAAS-8931: Added readable short description to final stat name representing the test case
    
    * STAAS-8930: Adds a formatted date string to the commit hash tier.  Allows for sorting result sets by date

--- lib/rspec_profiling/collectors/statsd.rb
+++ lib/rspec_profiling/collectors/statsd.rb
@@ -8,6 +8,9 @@ module RspecProfiling
     class Statsd
       #delegate :timing, :increment, :count, :gauge, :time, :batch, to: :statsd, allow_nil: true 
       NAMESPACE = 'rspec_profiling'
+
+      DATE_FORMAT_INPUT = '%a %b %d %H:%M:%S %Y'
+      DATE_FORMAT_OUTPUT = '%Y%m%dT%H%M%S'
       #Property used by CSV Collector, leaving it here
       #to maintain reference of available properties.
       HEADERS = %w{
@@ -36,17 +39,21 @@ module RspecProfiling
         new.uninstall
       end
 
-      def health(host, port)
-        flags = host =~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/ ? '-vuzn' : '-vuz'
+      def health(host, port, protocol)
+        flags = host =~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/ ? '-vzn' : '-vz'
+        case protocol
+          when :udp
+            flags << 'u'
+        end
         result = `nc #{flags} #{host} #{port} 2>&1 > /dev/null`
         result =~ /open|succe(ss|eded)/
       end
 
       def health_check
-        if self.health(RspecProfiling.config.statsd_host, RspecProfiling.config.statsd_port) 
-          @logger.info("rspec_profiling connection test to statsd UDP socket at [#{RspecProfiling.config.statsd_host}:#{RspecProfiling.config.statsd_port}] is successful.")
+        if self.health(RspecProfiling.config.statsd_host, RspecProfiling.config.statsd_port, RspecProfiling.config.statsd_protocol) 
+          @logger.info("rspec_profiling connection test to statsd socket at [#{RspecProfiling.config.statsd_host}:#{RspecProfiling.config.statsd_port}] is successful.")
         else
-          @logger.warn("rspec_profiling cannot connect to statsd UDP socket at [#{RspecProfiling.config.statsd_host}:#{RspecProfiling.config.statsd_port}]. You will not see profiling results.")
+          @logger.warn("rspec_profiling cannot connect to statsd socket at [#{RspecProfiling.config.statsd_host}:#{RspecProfiling.config.statsd_port}]. You will not see profiling results.")
         end
       end
 
@@ -62,10 +69,11 @@ module RspecProfiling
         self.statsd.decrement('profiling_live')
       end
 
-
       def initialize
         RspecProfiling.config.statsd_host ||= '127.0.0.1'
         RspecProfiling.config.statsd_port ||= '8125'
+        RspecProfiling.config.statsd_max_depth ||= 0
+        RspecProfiling.config.statsd_protocol ||= :udp
         @results = Array.new
         @resultType = Struct.new(:description, :process_time)
         @statsd = nil
@@ -75,25 +83,77 @@ module RspecProfiling
       def statsd
         return @statsd unless @statsd.nil?
         self.health_check
-        @statsd = ::Statsd.new(RspecProfiling.config.statsd_host, RspecProfiling.config.statsd_port).tap do |sd|
+        @statsd = ::Statsd.new(RspecProfiling.config.statsd_host, RspecProfiling.config.statsd_port, RspecProfiling.config.statsd_protocol).tap do |sd|
           sd.namespace = "ldxe.#{NAMESPACE}.app"
         end
       end
       
-      def formatDesc(description)
+      def format_desc(description)
         ::Digest::SHA1.hexdigest(description)[8..15]
       end
 
-      def formatFile(path) 
-        path.gsub('/', '.').gsub(/\.rb$|^\.[^.]*\./, '')
+      def format_file(path, max = 0) 
+        str = path.gsub('/', '.').gsub(/\.rb$|^\.[^.]*\./, '')
+        if max > 0
+          parts = str.split('.')
+          str = (parts[0..max-1] << parts[-1]).join('.')
+        end
+        return str
+      end
+
+      def format_date(date)
+        date.strftime(DATE_FORMAT_OUTPUT)
+      end
+
+      def build_stamp(desc, line_number)
+        hash_desc = format_desc(desc)
+        readable_short = format_readable_short(desc)
+        "#{line_number}_#{hash_desc}_#{readable_short}"
+      end
+
+      def format_readable_short(description) 
+        result = description.split(' ')
+        if result.length > 3
+          #Look for larger words to avoid prepositions and articles.
+          selected = result.select { |short| short.length > 3 }
+          #If this results in less than 3 words then use the original array.
+          selected = selected.length >= 3 ? selected : result
+          #Shorten to the last 3
+          result = shorten_array(selected, 3)
+        end
+        return result.join('_')
+      end
+
+      def shorten_array(arr, max)
+        max = arr.length if arr.length < max
+        arr[-max..-1] 
+      end
+
+      def format_readable_short(description) 
+        result = description.split(' ')
+        if result.length > 3
+          #Look for larger words to avoid prepositions and articles.
+          selected = result.select { |short| short.length > 3 }
+          #If this results in less than 3 words then use the original array.
+          selected = selected.length >= 3 ? selected : result
+          #Shorten to the last 3
+          result = shorten_array(selected, 3)
+        end
+        return result.join('_')
+      end
+
+      def shorten_array(arr, max)
+        max = arr.length if arr.length < max
+        arr[-max..-1] 
       end
 
       def insert(attributes)
         hash = attributes.fetch(:commit_hash)[0..7]
-        stamp = formatDesc(attributes.fetch(:description))
-        path = formatFile(attributes.fetch(:file))
+        stamp = build_stamp(attributes.fetch(:description), attributes.fetch(:line_number))
+        path = format_file(attributes.fetch(:file))
         branch = attributes.fetch(:branch)
-        key = "#{branch}.#{hash}.#{path}.#{stamp}".gsub("\n", '')
+        commit_date = format_date(attributes.fetch(:date))
+        key = "#{branch}.#{commit_date}_#{hash}.#{path}.#{stamp}".gsub("\n", '')
 
         self.statsd.batch do |b|
           b.timing("#{key}.process_time", attributes.fetch(:time))
--- spec/collectors/statsd.rb
+++ spec/collectors/statsd.rb
@@ -15,7 +15,7 @@ module RspecProfiling
           collector.insert({
             branch: 'master',
             commit_hash: 'ABC123',
-            date: 'Thu Dec 18 12:00:00 2012',
+            date: Time.new,
             file: '/some/file.rb',
             line_number: 10,
             description: 'Some spec',
@@ -34,19 +34,49 @@ module RspecProfiling
         end
 
         it 'Converts small description to 8 character hash' do
-          small = collector.formatDesc('test')
+          small = collector.format_desc('test')
           expect(small.length).to eq(8)
           expect(small).to eq('ccb19ba6')
         end
 
         it 'Converts large description to 8 character hash' do 
-          big = collector.formatDesc('test more strings with spaces_and_underscores')
+          big = collector.format_desc('test more strings with spaces_and_underscores')
           expect(big).to eq('a6bb61fe')
         end
 
         it 'Converts file path to use dot separator over forward slash' do 
-          str = collector.formatFile('/spec/test/some_test.rb')
+          str = collector.format_file('/spec/test/some_test.rb')
           expect(str).to eq 'test.some_test'
+        end
+
+        it 'Correctly uses the max depth parameter to top tier' do 
+          str = collector.format_file('/spec/test/shared/model/lib/controller/bulk_action.rb', 1)
+          expect(str).to eq 'test.bulk_action'
+        end
+
+        it 'Correctly uses max depth parameter to bottom tier' do
+          str = collector.format_file('/spec/test/shared/model/lib/controller/bulk_action.rb', 4)
+          expect(str).to eq 'test.shared.model.lib.bulk_action'
+        end
+        
+        it 'Creates a readable stamp from a long string' do
+          str = collector.build_stamp('API There are long strings in the test description where it can be really length however we need a small but unique name', 101)
+          expect(str).to eq '101_de72bdae_small_unique_name'
+        end
+
+        it 'Creates a readable stamp from a single word' do
+          str = collector.build_stamp('API', 101)
+          expect(str).to eq '101_0fbef1b4_API'
+        end
+
+        it 'Creates a readable stamp from a three word sentence' do
+          str = collector.build_stamp('There be dragons', 101)
+          expect(str).to eq '101_0de17114_There_be_dragons'
+        end
+
+        it 'Prefixes git commit hash with commit date' do
+          result = collector.results.first.description.match(/\d{8}T\d{6}/)
+          expect(result).not_to eq nil
         end
       end
     end
